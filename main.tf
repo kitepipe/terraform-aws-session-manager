a1e2404eb4b333723d3f5401865b97235e086f7a
Tim Kidd <tkidd77@Tims-Mac-Studio.local>
2022-07-06 17:58:03 -0500

    enabled Cloudwatch Streaming

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "aws_ssm_document" "session_manager_prefs" {
       s3EncryptionEnabled         = var.enable_log_to_s3 ? "true" : "false"
       cloudWatchLogGroupName      = var.enable_log_to_cloudwatch ? aws_cloudwatch_log_group.session_manager_log_group.name : ""
       cloudWatchEncryptionEnabled = var.enable_log_to_cloudwatch ? "true" : "false"
+      cloudWatchStreamingEnabled  = var.enable_log_to_cloudwatch ? "true" : "false"
       kmsKeyId                    = aws_kms_key.ssmkey.key_id
       shellProfile = {
         linux   = var.linux_shell_profile == "" ? var.linux_shell_profile : ""
