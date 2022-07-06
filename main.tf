c55c0cc9087265ee9aeb7e1fcd60e6ba45f276ab
Tim Kidd <tkidd77@Tims-Mac-Studio.local>
2022-07-06 18:22:00 -0500

    enabled longer session timeout

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "aws_ssm_document" "session_manager_prefs" {
       cloudWatchLogGroupName      = var.enable_log_to_cloudwatch ? aws_cloudwatch_log_group.session_manager_log_group.name : ""
       cloudWatchEncryptionEnabled = var.enable_log_to_cloudwatch ? "true" : "false"
       cloudWatchStreamingEnabled  = var.enable_log_to_cloudwatch ? "true" : "false"
+      idleSessionTimeout          = "60"
       kmsKeyId                    = aws_kms_key.ssmkey.key_id
       shellProfile = {
         linux   = var.linux_shell_profile == "" ? var.linux_shell_profile : ""
